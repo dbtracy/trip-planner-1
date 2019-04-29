d762904f0770a3a45801f382e5b65b130f028e8d
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-04-29 12:03:59 -0400

    added webpack and connected to mapbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('HELLO FROM JAVASCRIPT');
+const mapboxgl = require("mapbox-gl")
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZGFudHJhY3kiLCJhIjoiY2p2MmptbDVxMDNwNjQ0bno2c2xvYTVubyJ9.7qXQ7dYRVXrHA52FL4SSAQ"
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+})
